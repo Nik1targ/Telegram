a59c0830776565f4e1504c12c74f7f1600bffab8
Nik1targ <roan.forge@mail.ru>
2026-04-06 19:03:16 +0300

    add file

--- scripts/Telegram.js
+++ scripts/Telegram.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-=======
+ 
+
 // document.getElementById('themeToggle').addEventListener('click', function() {
 //     const currentTheme = document.body.className;
 //     if (currentTheme === 'light-theme') {
@@ -8,4 +8,4 @@
 //         document.body.className = 'light-theme';
 //     }
 // });
->>>>>>> 5ba2aa29cee82d94caf2ee4ff9f4db2772784e87
+
